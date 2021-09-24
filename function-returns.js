8a197a367fe9d2e924350598e7d15b6bcd6dd6ef
BryanOri <bryanorihuela@computingforall.org>
2021-09-23 18:03:53 -0800

    stuff

--- function-returns.js
+++ function-returns.js
@@ -1,13 +1,14 @@
 function ConvertDegreesFToC(degreesF)
 {
 	const degreesC = (degreesF - 32) * (5/9);
+	return degreesC
 
 	// 2) We need to give back the Celsius degrees to whomever
 	//      invoked the function, not just calculate them
 }
 
 let temperatureInF = 76;
-let temperatureInC;
+let temperatureInC = ConvertDegreesFToC(76)
 
 // 1) We need to invoke our conversion function and
 //      store the given-back degrees into "temperatureInC";
